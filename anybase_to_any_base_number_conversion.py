dce193a8103bea5beeb6fa86f2f1668d3a342ed5
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-28 19:49:10 +0530

    Create anybase_to_any_base_number_conversion.py
    converts number from any base to any base as user wants

---
+++ anybase_to_any_base_number_conversion.py
@@ -0,0 +1,28 @@
+while True:
+	j=int(input('frm which number system:'))
+	i=int(input('to which number system:'))
+	n=int(input('enter  number:'))
+	n1=n
+	p=1
+	s=0
+	while n>0:
+		r=n%i
+		x=int(r*p)
+		s=(s+x)
+		p=p*j
+		n=n/i	
+	#below statements are for checking user has enter number in right number system or not		
+	s2=s
+	p=1
+	s1=0	
+	while s>0:
+		r=s%j
+		x=int(r*p)
+		s1=s1+x
+		p=p*i
+		s=s/j
+	if s1==n1:
+		print(s2)
+	else:
+		print('enter a valid number plz')
+		
